670b2aaa8b04937fecf3d5cca6ed0913cf176c91
je941004 <je941004@naver.com>
2021-10-04 13:30:42 +0900

    test: add BaseBallGameControllerTest isContinue function test
    1이면 continue true, 2이면 continue false 테스트

---
+++ src/test/java/baseball/controller/BaseBallGameControllerTest.java
@@ -0,0 +1,24 @@
+package baseball.controller;
+
+import nextstep.utils.Console;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.MockedStatic;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
+
+public class BaseBallGameControllerTest {
+
+    @DisplayName("1이면 True, 2면 False")
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,false"})
+    void 숫자_1이면_true_2면_false(String input, boolean isContinue) {
+        try (final MockedStatic<Console> mockConsole = mockStatic(Console.class)) {
+            mockConsole.when(() -> Console.readLine())
+                    .thenReturn(input);
+            assertThat(BaseBallGameController.isGameContinue()).isEqualTo(isContinue);
+        }
+    }
+}
